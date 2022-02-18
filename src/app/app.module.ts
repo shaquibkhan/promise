ae37c536cc324c2c655683e86935f8286710db19
shaquib khan <connectshaquib@gmail.com>
2022-02-19 01:34:36 +0530

    promise implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromiseComponent } from './promise/promise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromiseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/promise/promise.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promise',
+  templateUrl: './promise.component.html',
+  styleUrls: ['./promise.component.sass']
+})
+export class PromiseComponent implements OnInit {
+
+
+  constructor() { }
+
+ 
+  dellAvailable(){
+  return true
+  }
+
+  hpAvailable(){
+  return false
+  }
+ 
+  promiseVal;
+
+  dell ={
+    name : 'Dell',
+    HardDisk : '2TB',
+    color : 'Black'
+  }
+
+  hp ={
+    name : 'Dell',
+    HardDisk : '2TB',
+    color : 'Black'
+  }
+
+  notAvailable ={
+    name : 'Dell',
+    HardDisk : '2TB',
+    color : 'Black'
+  }
+ 
+
+
+  ngOnInit(): void {
+  let buyLaptop = new Promise((resolve,reject)=>{
+    if(this.dellAvailable()){
+      return setTimeout(()=>{
+        resolve(this.dell);
+      },3000)
+     
+    } else if(this.hpAvailable()){
+      setTimeout(()=>{
+        resolve(this.hp);
+      },3000)
+      
+    }else{
+      setTimeout(()=>{
+        reject(this.notAvailable);
+      },3000)
+    }
+  });
+
+  buyLaptop.then(res=>{
+    console.log("then code=>", res);
+    this.promiseVal = res;
+  }).catch(res=>{
+    console.log("catch code=>", res);
+    this.promiseVal = res;
+  })
+  }
+
+}
